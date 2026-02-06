dbf636ed1841e35717581c3c2e7146002c323d9c
karamdev1 <karamgaggi100@gmail.com>
2026-02-06 15:10:56 +0300

    Update

--- build.sh
+++ build.sh
@@ -7,30 +7,31 @@ fi
 
 # Load config
 if [ ! -f config ]; then
-    echo "[-] config file not found"
+    echo "\e[1;31m[-] config file not found\e[0m"
     exit 1
 fi
 
 source config
 
 # Compiling parameters and toolchain
+export ARCH=$(eval echo $ARCH)
+export CC=$(eval echo $CC)
+export LD=$(eval echo $LD)
+export OBJCOPY=$(eval echo $OBJCOPY)
+export AS=$(eval echo $AS)
+export NM=$(eval echo $NM)
+export STRIP=$(eval echo $STRIP)
+export OBJDUMP=$(eval echo $OBJDUMP)
+export READELF=$(eval echo $READELF)
+export CROSS_COMPILE=$(eval echo $CROSS_COMPILE)
+export CROSS_COMPILE_ARM32=$(eval echo $CROSS_COMPILE_ARM32)
+OUT_DIR=$(eval echo $OUT_DIR)
+DEFCONFIG=$(eval echo $DEFCONFIG)
+SAVEDCONFIG=$(eval echo $SAVEDCONFIG)
+export KCFLAGS=' -w -pipe -O3'
 export ANDROID_MAJOR_VERSION=r
-ARCH=$(eval echo $ARCH)
-CC=$(eval echo $CC)
-LD=$(eval echo $LD)
-OBJCOPY=$(eval echo $OBJCOPY)
-AS=$(eval echo $AS)
-NM=$(eval echo $NM)
-STRIP=$(eval echo $STRIP)
-OBJDUMP=$(eval echo $OBJDUMP)
-READELF=$(eval echo $READELF)
-CROSS_COMPILE=$(eval echo $CROSS_COMPILE)
-CROSS_COMPILE_ARM32=$(eval echo $CROSS_COMPILE_ARM32)
-KCFLAGS=' -w -pipe -O3'
-OUT_DIR=$(eval echo "$OUT_DIR")
-KDIR=$(pwd)
-DEFCONFIG=$(eval echo "$DEFCONFIG")
-SAVEDCONFIG=$(eval echo "$SAVEDCONFIG")
+export KCPPFLAGS=' -O3'
+export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 # Colors
 BOLDGREEN="\e[1;32m"
@@ -39,12 +40,13 @@ BOLDBLUE="\e[1;96m"
 ENDCOLOR="\e[0m"
 
 # Check if theres config
-if [ -f "$KDIR/$OUT_DIR/.config" ]; then
+if [ -f "$(pwd)/$OUT_DIR/.config" ]; then
     CONFIG_STATUS="${BOLDGREEN}Config present${ENDCOLOR}"
 else
     CONFIG_STATUS="${BOLDRED}No .config found${ENDCOLOR}"
 fi
 
+# Show gui
 function show_gui() {
 	clear
 	echo -e "\e[1;93m"
@@ -53,23 +55,35 @@ function show_gui() {
 	echo -e "${BOLDGREEN}Creator: ${BOLDRED}karamdev1${ENDCOLOR}"
 	echo -e "${BOLDGREEN}Kernel Config: ${CONFIG_STATUS}${ENDCOLOR}"
 	echo
-	echo -e "${BOLDGREEN}|-------------------${ENDCOLOR}Compile${BOLDGREEN}---------------------------|${ENDCOLOR}"
+	echo -e "${BOLDGREEN}|-------------------${ENDCOLOR}Kernel${BOLDGREEN}----------------------------|${ENDCOLOR}"
 	echo -e "${BOLDGREEN}|        ${ENDCOLOR}[${BOLDBLUE}1${ENDCOLOR}] Compile Kernel                           ${BOLDGREEN}|${ENDCOLOR}"
 	echo -e "${BOLDGREEN}|        ${ENDCOLOR}[${BOLDBLUE}2${ENDCOLOR}] Compile Module (Doesn't need prepare)    ${BOLDGREEN}|${ENDCOLOR}"
 	echo -e "${BOLDGREEN}|        ${ENDCOLOR}[${BOLDBLUE}3${ENDCOLOR}] Prepare Module                           ${BOLDGREEN}|${ENDCOLOR}"
-	echo -e "${BOLDGREEN}|-------------------${ENDCOLOR}Clean${BOLDGREEN}-----------------------------|${ENDCOLOR}"
-	echo -e "${BOLDGREEN}|        ${ENDCOLOR}[${BOLDBLUE}4${ENDCOLOR}] Clean Kernel                             ${BOLDGREEN}|${ENDCOLOR}"
+	echo -e "${BOLDGREEN}|        ${ENDCOLOR}[${BOLDBLUE}4${ENDCOLOR}] Clean Kernel (Clean & Mrproper)          ${BOLDGREEN}|${ENDCOLOR}"
 	echo -e "${BOLDGREEN}|        ${ENDCOLOR}[${BOLDBLUE}5${ENDCOLOR}] Apply Defconfig (Selection in Config)    ${BOLDGREEN}|${ENDCOLOR}"
 	echo -e "${BOLDGREEN}|        ${ENDCOLOR}[${BOLDBLUE}6${ENDCOLOR}] Apply Saved Config (Selection in Config) ${BOLDGREEN}|${ENDCOLOR}"
+	echo -e "${BOLDGREEN}|        ${ENDCOLOR}[${BOLDBLUE}7${ENDCOLOR}] Edit Config (MENUCONFIG)                 ${BOLDGREEN}|${ENDCOLOR}"
+	echo -e "${BOLDGREEN}|        ${ENDCOLOR}[${BOLDBLUE}8${ENDCOLOR}] Edit Config (NCONFIG)                    ${BOLDGREEN}|${ENDCOLOR}"
 	echo -e "${BOLDGREEN}|-------------------${ENDCOLOR}Script${BOLDGREEN}----------------------------|${ENDCOLOR}"
 	echo -e "${BOLDGREEN}|        ${ENDCOLOR}[${BOLDRED}E${ENDCOLOR}] Exit Builder                             ${BOLDGREEN}|${ENDCOLOR}"
+	echo -e "${BOLDGREEN}|        ${ENDCOLOR}[${BOLDBLUE}G${ENDCOLOR}] Open the creator's github page           ${BOLDGREEN}|${ENDCOLOR}"
 	echo -e "${BOLDGREEN}|-------------------${ENDCOLOR}End${BOLDGREEN}-------------------------------|${ENDCOLOR}"
 	echo
 }
 
+# Show message success or failed based on exit code
+function check_exit_code() {
+	local ret=$?
+	if [ $ret -eq 0 ]; then
+		echo -e "${BOLDGREEN}[+] Success${ENDCOLOR}"
+	else
+		echo -e "${BOLDRED}[-] Failed (exit code: $ret)${ENDCOLOR}"
+	fi
+}
+
 while true; do
 	show_gui
-	read action
+	read -p "Enter the action: " action
 
 	case $action in
 		1)
@@ -78,16 +92,22 @@ while true; do
 		2)
 			echo editing_config
 			;;
-		3)
-			echo cleaning
+		4)
+			echo -e "${BOLDGREEN}[+] Cleaning${ENDCOLOR}"
+			make -s -C "$(pwd)" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y clean -j"$(nproc)" && make -s -C "$(pwd)" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y mrproper -j"$(nproc)"
+			check_exit_code
 			;;
 		E)
 			echo -e "${BOLDRED}[!] Exiting!!${ENDCOLOR}"
 			break
 			;;
+		G)
+			echo -e "${BOLDGREEN}[+] Opening the creator's github page${ENDCOLOR}"
+			xdg-open https://github.com/karamdev1 > /dev/null 2>&1 &
+			;;
 		*)
 			echo -e "${BOLDRED}[!] Invalid Action!!${ENDCOLOR}"
-			read -p temp "Press Enter to Continue"
 			;;
 	esac
+	read -p "Press Enter to continue..."
 done
